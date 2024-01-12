73e010ad068e8d2689e86ac8375fdeca89f77ee6
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2024-01-12 23:39:06 +0530

    1/12/2023 basic maths

--- DSA-2023/gcd.cpp
+++ DSA-2023/gcd.cpp
@@ -13,3 +13,30 @@ int calcGCD(int n, int m){
     int res=gcd(n,m);
     return res;
 }
+
+
+//second approach
+
+
+
+
+int calcGCD(int n, int m){
+    // Write your code here.
+   while(n>0 && m>0)
+   {
+       if(n>m)
+       {
+           n=n%m;
+       }
+       else{
+           m=m%n;
+       }
+   }
+   if(n==0)
+   {
+       return m;
+   }
+   else{
+       return n;
+   }
+}
