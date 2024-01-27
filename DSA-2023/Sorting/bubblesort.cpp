d3c29fdd3871257aa6138be98a00d0ac734f7d1b
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2024-01-28 00:53:43 +0530

    1/28/2024 sorting bubblesort

---
+++ DSA-2023/Sorting/bubblesort.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n) 
+{
+    //write your code here
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            
+        }
+    }
+}
