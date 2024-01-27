1f0da1da7e397298826754e028cb2cd6d7a637d1
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2024-01-28 01:19:31 +0530

    1/28/2024 sorting bubblesort

--- DSA-2023/Sorting/bubblesort.cpp
+++ DSA-2023/Sorting/bubblesort.cpp
@@ -8,6 +8,7 @@ void bubbleSort(vector<int>& arr, int n)
     //write your code here
     for(int i=n-1;i>0;i--)
     {
+        int didSwap=0;
         for(int j=0;j<i;j++)
         {
             if(arr[j]>arr[j+1])
@@ -15,8 +16,13 @@ void bubbleSort(vector<int>& arr, int n)
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                didSwap=1;
             }
             
+        }
+        if(didSwap==0)  //with this best case is derived. if the array is already sorted then we don't need to sort this again and again 
+        {
+            break;
         }
     }
 }
