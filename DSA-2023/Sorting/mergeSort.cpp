9617f2034f2fe2a05bfc896749a9b67d7630cb93
Sonu Kumar Gautam <sonukumargautam41638@gmail.com>
2024-01-28 23:09:36 +0530

    1/28/2024 sorting mergesort

---
+++ DSA-2023/Sorting/mergeSort.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void merge(vector<int> &arr,int l, int m, int r)
+{
+    vector<int>v;
+    int i=l;
+    int j=m+1;
+
+    while(i<=m && j<=r)
+    {
+        if(arr[i]<=arr[j])
+        {
+            v.push_back(arr[i]);
+            i++;
+            
+        }
+        else
+        {
+            v.push_back(arr[j]);
+            j++;
+        
+        }
+    }
+
+    while(i<=m)
+    {
+        v.push_back(arr[i]);
+        i++;
+        
+    }
+    while(j<=r)
+    {
+        v.push_back(arr[j]);
+        j++;
+    
+    }
+
+    for(int i=l;i<=r;i++)
+    {
+        
+        arr[i]=v[i-l];
+    }
+   
+}
+
+void mergeSort(vector<int> &arr, int l, int r) {
+    // Write Your Code Here
+
+    if(l>=r)
+    {
+        return;
+    }
+    int m=(l+r)/2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+    
+}
